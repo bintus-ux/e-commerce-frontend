b2bb002bc3c94c6e4539c94d522361d10587e47e
BintusCal <cezerike@gmail.com>
2023-06-12 00:50:20 +0100

    commit

--- src/Components/ProductCarousel.js
+++ src/Components/ProductCarousel.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { Carousel, CarouselItem, Image } from 'react-bootstrap'
+import { Carousel, Container, Image } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Loader from './Loader'
 import Message from './Message'
@@ -21,20 +21,22 @@ const ProductCarousal = () => {
   ) : error ? (
     <Message variant='danger'>{error}</Message>
   ) : (
-    <Carousel pause='hover' className='bg-dark'>
-      {products.map((product) => (
-        <CarouselItem key={product._id}>
-          <Link to={`/product/${product._id}`}>
-            <Image src={product.image} alt={product.name} fluid />
-            <Carousel.Caption className='carousel-caption'>
-              <h2>
-                {product.name} (${product.price})
-              </h2>
-            </Carousel.Caption>
-          </Link>
-        </CarouselItem>
-      ))}
-    </Carousel>
+    <Container fluid>
+      <Carousel pause='hover' className='bg-dark'>
+        {products.map((product) => (
+          <Carousel.Item key={product._id}>
+            <Link to={`/product/${product._id}`}>
+              <Image src={product.image} alt={product.name} fluid />
+              <Carousel.Caption className='carousel-caption'>
+                <h2>
+                  {product.name} (${product.price})
+                </h2>
+              </Carousel.Caption>
+            </Link>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </Container>
   )
 }
 
